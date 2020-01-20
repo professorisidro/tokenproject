df1a40541523ae4016a70f86133de2e38473adaf
Professor Isidro <isidro@professorisidro.com.br>
2020-01-20 16:04:43 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ListaComponent } from './componentes/lista/lista.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'lista', component: ListaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TituloComponent } from './componentes/titulo/titulo.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ListaComponent } from './componentes/lista/lista.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { UsuarioService } from './servicos/usuario.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent,
+    LoginComponent,
+    ListaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
